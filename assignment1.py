33b02e40d81850f5798fabebfbf266669f639724
Prabhakant Akshay Sanjeev <akshayprabhakant@gmail.com>
2019-11-08 18:14:57 +0530

    Added all files

---
+++ assignment1.py
@@ -0,0 +1,42 @@
+import geopandas as gpd
+import numpy as np
+from matplotlib import path
+import matplotlib.pyplot as plt
+
+def giveMask():
+	#fileName = "Aqueduct_river_basins_TAPTI RIVER.shp"
+	fileName = "/home/laferrari/IIIT-H SEMESTERS/4-1/hydrological_modelling/assignment1/Aqueduct_river_basins_TAPTI RIVER.shp"
+	shape = gpd.read_file(fileName)
+	shapeArr = [i for i in shape.geometry]
+	x,y = shapeArr[0].exterior.coords.xy
+
+	left = min(x) -0.5
+	right = max(x) +0.5
+	up = max(y) +0.5
+	down = min(y) -0.5
+	resolv = 0.02
+
+	len1, len2 = int((right-left) / resolv)+1, int((up-down) / resolv)+1 
+	zeroes = [ [0 for i in range(len2)] for j in range(len1)]
+	a1, a2 = [left + resolv*i for i in range(len1)], [down + resolv*i for i in range(len2)]
+	p = path.Path([(x[i], y[i]) for i in range(len(x))])
+
+	for i in range(len1):
+		for j in range(len2):
+			if p.contains_points([(a1[i], a2[j])]):
+				zeroes[i][j] = 1
+			else:
+				zeroes[i][j] = 0
+
+	zeroes = np.array(zeroes).T.tolist()
+	plt.figure(0)
+	plt.imshow(zeroes)
+	plt.set_cmap('gray')
+	plt.ylim((0,150))
+
+	plt.figure(1)
+	plt.plot(x,y)
+	plt.show()
+
+if __name__ == '__main__':
+	giveMask()
---
+++ main.py
@@ -0,0 +1,70 @@
+import os
+from tkinter import Tk, Menu, Button, Message, Toplevel, filedialog
+top = Tk()
+top.geometry("500x500")
+top.title("Course Project")
+menubar = Menu(top)
+
+file = Menu(menubar, tearoff=0)
+file.add_command(label="New")
+file.add_command(label="Open")
+file.add_command(label="Save")
+file.add_command(label="Save As...")
+file.add_command(label="Close")
+file.add_separator()
+file.add_command(label="Exit", command=top.quit)
+
+menubar.add_cascade(label="File", menu=file)
+
+edit = Menu(menubar, tearoff=0)  
+edit.add_command(label="Undo")  
+  
+edit.add_separator()  
+  
+edit.add_command(label="Cut")  
+edit.add_command(label="Copy")  
+edit.add_command(label="Paste") 
+edit.add_command(label="Delete")  
+edit.add_command(label="Select All")  
+menubar.add_cascade(label="Edit", menu=edit)
+
+def about():
+    about_window = Toplevel(top)
+    about_window.geometry("600x200")
+    msg = Message(about_window, width=500,  text="1. This program can generate a mask file by reading only the shape file of a basin.\n\n\
+2. It can also perform watershed delineation by reading a DEM geotiff file and mask file of a river basin")
+    msg.pack()
+    about_window.mainloop()
+
+def get_file():
+    cwd = os.getcwd()
+    fileObj = filedialog.askopenfile(initialdir=cwd,
+                           filetypes =(("Shape File", "*.shp"),("All Files","*.*")),
+                           title = "Choose a file.")
+    filename = fileObj.name
+    print(filename)
+
+
+    
+
+def shapeToMask():
+    new_window = Toplevel(top)
+    new_window.geometry("200x200")
+    txt = Message(new_window, width=150, text="Choose a shape file")
+    txt.pack()
+    browse_button = Button(new_window, text="Browse..", command=get_file)
+    browse_button.place(x = 40, y = 50)
+    new_window.mainloop()
+
+  
+options = Menu(menubar, tearoff=0)
+options.add_command(label="Generate Mask file", command=shapeToMask)
+menubar.add_cascade(label="Options", menu=options)
+
+help = Menu(menubar, tearoff=0)  
+help.add_command(label="About", command=about)  
+menubar.add_cascade(label="Help", menu=help)  
+
+
+top.config(menu=menubar)  
+top.mainloop()  
